d20d921d4b4867f79754ca88322258d478a9b827
Elijah Goh <elijah.goh@coverwallet.com>
2022-04-17 16:08:49 +0800

    Update

--- lib/sequel/plugins/nanoid.rb
+++ lib/sequel/plugins/nanoid.rb
@@ -1,41 +1,43 @@
 # frozen_string_literal: true
 
-module Sequel::Plugins
-  module Nanoid
-    def self.configure(model, opts = OPTS)
-      model.instance_exec do
-        @nanoid_field = opts[:field] || :nanoid
-        @nanoid_overwrite = opts[:force] || false
-      end
-    end
-
-    module ClassMethods
-      attr_reader :nanoid_field
-
-      def nanoid_overwrite?
-        @nanoid_overwrite
+module Sequel
+  module Plugins
+    module Nanoid
+      def self.configure(model, opts = OPTS)
+        model.instance_exec do
+          @nanoid_field = opts[:field] || :nanoid
+          @nanoid_overwrite = opts[:force] || false
+        end
       end
 
-      Plugins.inherited_instance_variables(self, :@nanoid_field => nil, :@nanoid_overwrite => nil)
-    end
+      module ClassMethods
+        attr_reader :nanoid_field
 
-    module InstanceMethods
-      def before_validation
-        set_nanoid if new?
-        super
+        def nanoid_overwrite?
+          @nanoid_overwrite
+        end
+
+        Plugins.inherited_instance_variables(self, :@nanoid_field => nil, :@nanoid_overwrite => nil)
       end
 
-      private
+      module InstanceMethods
+        def before_validation
+          set_nanoid if new?
+          super
+        end
 
-      def create_nanoid
-        ::Nanoid.generate
-      end
+        private
 
-      def set_nanoid(nanoid = create_nanoid)
-        field = model.nanoid_field
-        meth = :"#{field}="
-        if respond_to?(field) && respond_to?(meth) && (model.nanoid_overwrite? || get_column_value(field).nil?)
-          set_column_value(meth, nanoid)
+        def create_nanoid
+          ::Nanoid.generate
+        end
+
+        def set_nanoid(nanoid = create_nanoid)
+          field = model.nanoid_field
+          meth = :"#{field}="
+          if respond_to?(field) && respond_to?(meth) && (model.nanoid_overwrite? || get_column_value(field).nil?)
+            set_column_value(meth, nanoid)
+          end
         end
       end
     end
--- sequel_nanoid.gemspec
+++ sequel_nanoid.gemspec
@@ -6,17 +6,16 @@ Gem::Specification.new do |spec|
   spec.authors = ["Elijah Goh"]
   spec.email = ["iamchoonggg@gmail.com"]
 
-  spec.summary = "Use NanoID in your sequel gem"
-  spec.description = "TODO: Write a longer description or delete this line."
-  spec.homepage = "TODO: Put your gem's website or public repo URL here."
+  spec.summary = "Use Nanoid in your sequel gem"
+  spec.description = "Use nanoid with sequel https://github.com/radeno/nanoid.rb"
+  spec.homepage = "https://github.com/elithecho/sequel_nanoid"
   spec.license = "MIT"
   spec.required_ruby_version = ">= 2.6.0"
 
-  spec.metadata["allowed_push_host"] = "TODO: Set to your gem server 'https://example.com'"
+  spec.metadata["allowed_push_host"] = "https://rubygems.org"
 
   spec.metadata["homepage_uri"] = spec.homepage
-  spec.metadata["source_code_uri"] = "TODO: Put your gem's public repo URL here."
-  spec.metadata["changelog_uri"] = "TODO: Put your gem's CHANGELOG.md URL here."
+  spec.metadata["source_code_uri"] = "https://github.com/radeno/nanoid.rb"
 
   # Specify which files should be added to the gem when it is released.
   # The `git ls-files -z` loads the files in the RubyGem that have been added into git.
@@ -34,4 +33,5 @@ Gem::Specification.new do |spec|
 
   # For more information and examples about making a new gem, check out our
   # guide at: https://bundler.io/guides/creating_gem.html
+  # spec.metadata["rubygems_mfa_required"] = "true"
 end
